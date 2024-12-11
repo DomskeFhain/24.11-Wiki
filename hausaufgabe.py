3ef4e0cf0d89fb26f45ae2247c3b26895e3c6692
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-11 15:28:46 +0100

    Create hausaufgabe.py

---
+++ hausaufgabe.py
@@ -0,0 +1,23 @@
+a = 99
+b = 66
+
+print("a =", a)
+print("b =", a)
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+
+note1 = 3
+note2 = 6
+note3 = 5
+summe = 14
+anzahl = 3
+
+print("Note 1 =", note1)
+print("Note 2 =", note2)
+print("Note 3 =", note3)
+
+print("Anzahl aller Notenpunkte =", note1 + note2 + note3)
+print("Summe / Anzahl = Durchscnitt = ", summe / anzahl)
