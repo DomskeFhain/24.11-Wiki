22721d94f5888b8820ca64874d92511bd795d0a1
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-11 15:43:29 +0100

    Create Zusatzaufgabe.py

---
+++ Zusatzaufgabe.py
@@ -0,0 +1,5 @@
+meile = 0.621371
+print("Aufgabe 3.3 Zusatzaufgabe")
+kilometer = float(input("Gib die Kilometer als Zahl ein, die umgerechnet werden sollen: "))
+meilen = kilometer * meile
+print(f"{kilometer} Kilometer entsprechen " + str(meilen) +" Meilen.")
