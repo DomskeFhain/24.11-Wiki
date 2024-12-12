b2a0406580d2b9e110a9bdb205900788c2e3800f
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-12 14:48:43 +0100

    Create Hausaufgabe2.py

---
+++ Hausaufgabe2.py
@@ -0,0 +1,18 @@
+# 1
+
+
+Vorname = input("Wie ist dein Vorname: ")
+Nachname = input("Wie ist dein Nachname: ")
+
+print(f"Der Name von dir lautet: ", (Vorname + " " + Nachname ))
+
+Zahl1 = input("Was ist Zahl 1 was addiert werden soll: ")
+Zahl2 = input("Was ist Zahl 2 was addiert werden soll: ")
+
+Zahl1_int = int(Zahl1)
+Zahl2_int = int(Zahl2)
+
+addition_zahl1_int_zahl2_int = Zahl1_int + Zahl2_int
+
+
+print(f"Die Summe der Zahlen lautet: {addition_zahl1_int_zahl2_int}")
