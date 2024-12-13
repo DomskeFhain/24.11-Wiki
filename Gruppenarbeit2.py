e77091ed090faa1277b82452f8a586d04556c533
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-13 13:49:21 +0100

    Create Gruppenarbeit2.py

---
+++ Gruppenarbeit2.py
@@ -0,0 +1,18 @@
+a = 69
+b = 96
+
+print("AUFAGBE 1")
+print("a < b", (a < b))
+print("a > b", (a > b))
+print("a == b", (a == b))
+print("a != b", (a != b))
+print("a >= b", (a >= b))
+print("a <= b", (a <= b))
+
+print("AUFGABE 2")
+print("a < b and a > b", (a < b) and (a > b))
+print("a > b and a < b", (a > b) and (a < b))
+print("a < b and a > b", (a < b) or (a > b))
+print("a > b and a < b", (a > b) or (a < b))
+print("a < b and a > b", (not(a < b)))
+print("a > b and a < b", (not(a > b)))
